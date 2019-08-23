5e5b41f36ca4636cc77f5829aaea5f4ab0fd54de
Timofey <tim361234567890@gmail.com>
2019-08-23 08:47:27 +0300

    Первый комит и добавление экрана загрузки.

---
+++ DodoTest Helper/DodoTest Helper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DodoTest Helper
+//
+//  Created by Timofey Koloshenko on 23/08/2019.
+//  Copyright © 2019 Timofey Koloshenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
